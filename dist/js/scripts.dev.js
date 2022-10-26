a0ac29b3701cfb9278dc3f61d4daa69392ca04b7
angelo <angelo@fluxton.co.uk>
2022-10-26 18:54:54 +0100

    slow progress

---
+++ dist/js/scripts.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/// aQuery
+function $all(elements) {
+  return document.querySelectorAll(elements);
+}
+
+function $id(element) {
+  return document.querySelector(element);
+}
+
+function $log(message) {
+  console.log(message);
+}
+
+function $hide(element) {
+  element.style.display = 'none';
+  element.classList.remove('active');
+}
+
+function $show(element) {
+  element.classList.add('active');
+} //end aQuery
