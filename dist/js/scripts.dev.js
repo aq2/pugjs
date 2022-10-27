f148f737e738b558e37a48af2b9e5554fc410aaf
angelo <angelo@fluxton.co.uk>
2022-10-27 19:41:11 +0100

    still fuckin around

--- dist/js/scripts.dev.js
+++ dist/js/scripts.dev.js
@@ -2,31 +2,67 @@
 
 /// stretch
 var stretch = $get('#stretch');
-myMove();
+var header = $get('header');
+var headline = $get('h1');
+var nav = $get('nav');
+var main = $get('main'); // let stretchHeight = stretch.height
 
-function myMove() {
-  var id = null;
-  var elem = document.getElementById("om");
-  var pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 5);
+var stretchHeight = stretch.offsetHeight;
+var stretchWidth = stretch.width;
+var headerHeight = stretch.offsetHeight + nav.offsetHeight; // init()
 
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.top = pos + 'px';
-      elem.style.left = pos + 'px';
-    }
-  }
-}
+function init() {} // headline.style.transform = 'translateY(200px)'
+// nav.style.transform = 'translateY(436px)'
+// console.log(stretch.offsetHeight)
+// nav.style.transform = 'translateY(-' + stretch.offsetHeight +'px)'
+// main.style.transform = 'translateY(' + headerHeight + 'px)'
+// myMove()
+// function myMove() {
+//   let id = null;
+//   const elem = document.getElementById("om");
+//   let pos = 0;
+//   clearInterval(id);
+//   id = setInterval(frame, 5);
+//   function frame() {
+//     if (pos == 2350) {
+//       clearInterval(id);
+//     } else {
+//       pos++;
+//       elem.style.top = pos + 'px';
+//       elem.style.left = pos + 'px';
+//     }
+//   }
+// }
+
+
+window.onresize = function (e) {
+  var stretchWidth = stretch.width;
+  console.log(stretchWidth);
+};
+
+var delay = 150;
+var range = 'XXX';
+var timeout = false;
 
 window.onscroll = function (e) {
-  // scrollIt()
-  // console.log(document.documentElement.scrollTop)
-  stretch.style.transform = 'scale(0.4)';
+  clearTimeout(timeout);
+  timeout = setTimeout(scroller, delay);
 };
+
+function scroller() {
+  var scrollTop = document.documentElement.scrollTop;
+  var stretchHeight = stretch.offsetHeight;
+  var scaleFactor = (stretchHeight - scrollTop) / stretchHeight; // console.log(scrollTop)
+  // console.log(stretchHeight)
+  // console.log(scaleFactor)
+  // todo maths nope 👇
+
+  stretch.style.transform = 'scale(' + scaleFactor + ')'; // console.log(parseInt(scrollFactor) + ' f')
+  // header.style.transform = 'scale(' + scrollFactor + '0.4)'
+  // header.style.transform = 'scale(' + scrollFactor + '0.4)'
+  // header.style.transform = 'scale(0.4)'
+  // init()
+}
 
 window.addEventListener('scroll', function () {// scrollIt()
 }); /// aQuery
--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,77 @@
 /// stretch
   const stretch = $get('#stretch')
-  myMove()
+  const header = $get('header')  
+  const headline = $get('h1')  
+  const nav = $get('nav')
+  const main = $get('main')
 
-  function myMove() {
-    let id = null;
-    const elem = document.getElementById("om");
-    let pos = 0;
-    clearInterval(id);
-    id = setInterval(frame, 5);
-    function frame() {
-      if (pos == 350) {
-        clearInterval(id);
-      } else {
-        pos++;
-        elem.style.top = pos + 'px';
-        elem.style.left = pos + 'px';
-      }
-    }
+  // let stretchHeight = stretch.height
+  let stretchHeight = stretch.offsetHeight
+  let stretchWidth = stretch.width
+  let headerHeight = stretch.offsetHeight + nav.offsetHeight
+  // init()
+
+function init() {
+  // headline.style.transform = 'translateY(200px)'
+  // nav.style.transform = 'translateY(436px)'
+  // console.log(stretch.offsetHeight)
+  // nav.style.transform = 'translateY(-' + stretch.offsetHeight +'px)'
+  // main.style.transform = 'translateY(' + headerHeight + 'px)'
+}
+
+
+  // myMove()
+    // function myMove() {
+    //   let id = null;
+    //   const elem = document.getElementById("om");
+    //   let pos = 0;
+    //   clearInterval(id);
+    //   id = setInterval(frame, 5);
+    //   function frame() {
+    //     if (pos == 2350) {
+    //       clearInterval(id);
+    //     } else {
+    //       pos++;
+    //       elem.style.top = pos + 'px';
+    //       elem.style.left = pos + 'px';
+    //     }
+    //   }
+    // }
+
+  window.onresize = function (e) {
+      let stretchWidth = stretch.width
+      console.log(stretchWidth)
   }
 
+  let delay = 150
+  let range = 'XXX'
+  let timeout = false
   window.onscroll = function (e) {
-    // scrollIt()
-    // console.log(document.documentElement.scrollTop)
-    stretch.style.transform = 'scale(0.4)'
+    clearTimeout(timeout)
+    timeout = setTimeout(scroller, delay)
   }
+
+  function scroller() {
+    let scrollTop = document.documentElement.scrollTop
+    let stretchHeight = stretch.offsetHeight
+    let scaleFactor = (stretchHeight - scrollTop) / stretchHeight
+    // console.log(scrollTop)
+    // console.log(stretchHeight)
+    // console.log(scaleFactor)
+    // todo maths nope 👇
+    stretch.style.transform = 'scale(' + scaleFactor + ')'
+    // console.log(parseInt(scrollFactor) + ' f')
+    // header.style.transform = 'scale(' + scrollFactor + '0.4)'
+    // header.style.transform = 'scale(' + scrollFactor + '0.4)'
+    // header.style.transform = 'scale(0.4)'
+    // init()
+  }
+
 
   window.addEventListener('scroll', () => {
     // scrollIt()
   })
+
 
 
 
