bf0e0074961584156cb5f75924b4c7d02bdd19a4
angelo <angelo@fluxton.co.uk>
2022-10-27 15:53:00 +0100

    fuckin around with position

--- dist/js/scripts.dev.js
+++ dist/js/scripts.dev.js
@@ -1,11 +1,41 @@
 "use strict";
 
-/// aQuery
+/// stretch
+var stretch = $get('#stretch');
+myMove();
+
+function myMove() {
+  var id = null;
+  var elem = document.getElementById("om");
+  var pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
+
+window.onscroll = function (e) {
+  // scrollIt()
+  // console.log(document.documentElement.scrollTop)
+  stretch.style.transform = 'scale(0.4)';
+};
+
+window.addEventListener('scroll', function () {// scrollIt()
+}); /// aQuery
+
 function $all(elements) {
   return document.querySelectorAll(elements);
 }
 
-function $id(element) {
+function $get(element) {
   return document.querySelector(element);
 }
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,43 @@
+/// stretch
+  const stretch = $get('#stretch')
+  myMove()
+
+  function myMove() {
+    let id = null;
+    const elem = document.getElementById("om");
+    let pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 5);
+    function frame() {
+      if (pos == 350) {
+        clearInterval(id);
+      } else {
+        pos++;
+        elem.style.top = pos + 'px';
+        elem.style.left = pos + 'px';
+      }
+    }
+  }
+
+  window.onscroll = function (e) {
+    // scrollIt()
+    // console.log(document.documentElement.scrollTop)
+    stretch.style.transform = 'scale(0.4)'
+  }
+
+  window.addEventListener('scroll', () => {
+    // scrollIt()
+  })
+
+
+
 /// aQuery
 
 function $all(elements) {
   return document.querySelectorAll(elements)
 }
 
-function $id(element) {
+function $get(element) {
   return document.querySelector(element)
 }
 
@@ -20,7 +53,5 @@ function $hide(element) {
 function $show(element) {
   element.classList.add('active')
 }
-
-
 
 //end aQuery
